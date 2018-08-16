66a84400dd4cfde6aa4829ba77f650a6c28cc6d8
kunallguptaaa <kunallguptaa@gmail.com>
2018-08-16 22:07:40 +0530

    Egyptian Paused

--- EgyptianFraction.py
+++ EgyptianFraction.py
@@ -1,10 +1,20 @@
 class EgyptianFraction(object):
     """docstring for EgyptianFraction."""
-    def __init__(self, arg):
+    def __init__(self):
         super(EgyptianFraction, self).__init__()
-        self.arg = arg
-print("Enter the no. of testcases\n")
-int testcases = input()
-for i in range(0, testcases):
+
+    def input_testcase(self):
+        elements = int(input())
+        fractions = list();
+        for i in range(elements):
+            fractions.append(int(input()))
+            
+
+
+
+print("Enter the no. of testcases")
+testcases = int(input())
+obj = EgyptianFraction()
+for i in range(testcases):
     print("Input no. of fractions")
-    elements = input()
+    obj.input_testcase();
