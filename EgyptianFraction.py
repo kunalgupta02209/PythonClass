8b9e971950b5e2aebfaaf6704675c378b8382103
kunallguptaaa <kunallguptaa@gmail.com>
2018-08-16 18:31:39 +0530

    Starting egyptian fraction and basic testing

---
+++ EgyptianFraction.py
@@ -0,0 +1,10 @@
+class EgyptianFraction(object):
+    """docstring for EgyptianFraction."""
+    def __init__(self, arg):
+        super(EgyptianFraction, self).__init__()
+        self.arg = arg
+print("Enter the no. of testcases\n")
+int testcases = input()
+for i in range(0, testcases):
+    print("Input no. of fractions")
+    elements = input()
