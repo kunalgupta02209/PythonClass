55c1b07942c0e99e745b48a0892748f7bb233e94
kunallguptaaa <kunallguptaa@gmail.com>
2018-10-01 00:27:32 +0530

    Changed the fraction storage into a class object and will be using the class object to easily manimulate the fraction data. Seems simpler

--- FractionSum.py
+++ FractionSum.py
@@ -2,29 +2,37 @@ class Fractions:
     def input_fractions(self):
         print("Enter no. of fractions to add")
         n = int(input())
-        numerator = list()
-        denominator = list()
+        fractions = list()
         for i in range(n):
             print("Input fraction no. ", i+1, " separated by '/'");
             frac = [int(i) for i in input().split('/')];
-            numerator.append(int(frac[0]));
-            denominator.append(int(frac[1]));
-        fractions = dict()
-        fractions["num"] = numerator
-        fractions["den"] = denominator
+            num = int(frac[0]);
+            den = int(frac[1]);
+            fractions.append(Fraction(num,den))
         return fractions
 
-    def compute(self, fractions):
-        num = fractions["num"]
-        den = fractions["den"]
-    def output_fractions(self, fractions):
-        num = fractions["num"]
-        den = fractions["den"]
-        for i in range(len(num)):
-            self.output_fraction(num[i],den[i])
-    def output_fraction(self, num, den):
-        print(num,"/",den)
+    def compute_sum_2_fracs(self, frac1, frac2):
+        num_sum = 0
+        den_prod = frac1.den*frac2.den
+        num_sum = frac1.den*frac2.num + frac2.den*frac1.num
 
+    def output_fractions(self, fractions):
+        for i in range(len(fractions)):
+            self.output_fraction(fractions[i])
+
+    def output_fraction(self, Fraction):
+        print(Fraction.num,"/",Fraction.den)
+
+    def reduce(self, fraction):
+        i = int(fraction.den/2)+1
+        while i > 1:
+            if ((fraction % i) == 0) and ((de
+
+
+class Fraction:
+    def __init__(self, num, den):
+        self.num = num
+        self.den = den
 def main():
     obj = Fractions()
     fractions = obj.input_fractions()
