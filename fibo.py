779341789260d6a96239e38c3dd240a50cc5a467
Kunal Gupta <kunallguptaa@gmail.com>
2019-02-08 18:40:55 +0530

    using iter and next

--- fibo.py
+++ fibo.py
@@ -1,15 +1,18 @@
 class fibo:
 	def __init__(self,n):
-		self.ar = []
-		self.genFib(n)
-	def __contains__(self,i):
-		return i in self.ar
+		self.n = n
+		self.fib = 0
+		self.b = 1
 	def __iter__(self):
-		return iter(self.ar)
-	def genFib(self,n):
-		self.ar.extend([0,1])
-		while(self.ar[-1] <= n):
-			self.ar.append(self.ar[-2]+self.ar[-1])
+		return self
+	def __next__(self):
+		if(self.fib > self.n):
+			raise StopIteration
+		else:
+			tmp = self.fib
+			self.fib = self.fib+self.b
+			self.b = tmp
+			return tmp
 n = 10
 for i in fibo(n):
     print(i)
